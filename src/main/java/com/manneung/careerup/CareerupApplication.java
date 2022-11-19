b5443a9916f8cd108df645c8c539045ec721c714
YuJin Kwon <tokyj515@gmail.com>
2022-11-19 11:50:26 +0900

    initial

---
+++ src/main/java/com/manneung/careerup/CareerupApplication.java
@@ -0,0 +1,13 @@
+package com.manneung.careerup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CareerupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CareerupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/manneung/careerup/CareerupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manneung.careerup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CareerupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
