2fd39f0fd060aaccd6042b9225e3d5f7c997fe28
YuJin Kwon <tokyj515@gmail.com>
2022-11-20 22:19:40 +0900

    [add] 메모리 사용량 출력 추가

--- src/main/java/com/manneung/careerup/CareerupApplication.java
+++ src/main/java/com/manneung/careerup/CareerupApplication.java
@@ -2,12 +2,21 @@ package com.manneung.careerup;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+
+//@EnableJpaAuditing -> JpaAuditingConfig
 @SpringBootApplication
 public class CareerupApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CareerupApplication.class, args);
+
+		// 메모리 사용량 출력
+		long heapSize = Runtime.getRuntime().totalMemory();
+		System.out.println("HEAP Size(M) : "+ heapSize / (1024*1024) + " MB");
 	}
+
 
 }
