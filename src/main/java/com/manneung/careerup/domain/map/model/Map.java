69e994035072f2a353c5dc38cafba23413427233
YuJin Kwon <tokyj515@naver.com>
2022-12-01 23:14:25 +0900

    [add] map 엔티티, 리포지토리 추가

---
+++ src/main/java/com/manneung/careerup/domain/map/model/Map.java
@@ -0,0 +1,25 @@
+package com.manneung.careerup.domain.map.model;
+
+
+import com.manneung.careerup.domain.base.BaseEntity;
+import lombok.*;
+
+import javax.persistence.*;
+
+@RequiredArgsConstructor
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@Entity(name = "map")
+public class Map extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "map_idx")
+    private int mapIdx;
+
+    @Column(nullable = false)
+    private String job;
+
+}
---
+++ src/main/java/com/manneung/careerup/domain/map/repository/MapRepository.java
@@ -0,0 +1,8 @@
+package com.manneung.careerup.domain.map.repository;
+
+import com.manneung.careerup.domain.map.model.Map;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MapRepository extends JpaRepository<Map, Integer> {
+
+}
