6760e7e1fbb281de7e69d1aa34b67ecd6a0763f6
YuJin Kwon <tokyj515@naver.com>
2022-12-09 17:06:22 +0900

    [fix] swagger 설정 고치기

---
+++ src/main/java/com/manneung/careerup/global/jwt/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,46 @@
+package com.manneung.careerup.global.jwt;
+
+import org.json.simple.JSONObject;
+import org.springframework.security.authentication.InternalAuthenticationServiceException;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Arrays;
+
+
+
+@Component
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException {
+        String exception = (String)request.getAttribute("exception");
+        if(exception == null) setResponse(response, JWTExceptionList.UNKNOWN_ERROR);
+            //잘못된 타입의 토큰인 경우
+        else if(exception.equals(JWTExceptionList.WRONG_TYPE_TOKEN.getErrorCode())) setResponse(response, JWTExceptionList.WRONG_TYPE_TOKEN);
+            //토큰 만료된 경우
+        else if(exception.equals(JWTExceptionList.EXPIRED_TOKEN.getErrorCode())) {
+            System.out.println("토큰만료된경우");
+            setResponse(response, JWTExceptionList.EXPIRED_TOKEN);
+        }
+        //지원되지 않는 토큰인 경우
+        else if(exception.equals(JWTExceptionList.UNSUPPORTED_TOKEN.getErrorCode())) setResponse(response, JWTExceptionList.UNSUPPORTED_TOKEN);
+        else setResponse(response, JWTExceptionList.ACCESS_DENIED);
+
+    }
+
+    private void setResponse(HttpServletResponse response, JWTExceptionList exceptionCode) throws IOException {
+        response.setContentType("application/json;charset=UTF-8");
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+
+        JSONObject responseJson = new JSONObject();
+        responseJson.put("message", exceptionCode.getMessage());
+        responseJson.put("errorCode", exceptionCode.getErrorCode());
+
+        response.getWriter().print(responseJson);
+    }
+}
