fde01ca295406499e7263b8b3640741aca384944
YuJin Kwon <tokyj515@naver.com>
2022-12-12 22:37:06 +0900

    [modify] 베이스 정리

--- src/main/java/com/manneung/careerup/domain/base/BaseResponse.java
+++ src/main/java/com/manneung/careerup/domain/base/BaseResponse.java
@@ -3,39 +3,33 @@ package com.manneung.careerup.domain.base;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import com.manneung.careerup.test.TestRes;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import static com.manneung.careerup.domain.base.BaseResponseStatus.SUCCESS;
 
 @Getter
 @AllArgsConstructor
-@JsonPropertyOrder({"isSuccess", "code", "message", "result"})
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+//@JsonPropertyOrder({"isSuccess", "code", "message", "result"})
 public class BaseResponse<T> {
     //API base응답
-    @JsonProperty("isSuccess")
-    private final Boolean isSuccess;
-    private final String message;
 
-    private final int code;
-    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private int code;
+    private  String message;
     private T result;
 
 
-
-    //요청 성공
-    public BaseResponse(T result) {
-        this.isSuccess = SUCCESS.isSuccess();
-        this.message = SUCCESS.getMessage();
-        this.code = SUCCESS.getCode();
-        this.result = result;
+    public static <T> BaseResponse<T> create(BaseResponseStatus baseResponseStatus) {
+        //return new BaseResponse<>(code, message, null);
+        return new BaseResponse<>(baseResponseStatus.getCode(), baseResponseStatus.getMessage(), null);
+    }
+    public static <T> BaseResponse<T> create(BaseResponseStatus baseResponseStatus, T result) {
+        return new BaseResponse<>(baseResponseStatus.getCode(), baseResponseStatus.getMessage(), result);
     }
 
-    //요청 실패
-    public BaseResponse(BaseResponseStatus status) {
-        this.isSuccess = status.isSuccess();
-        this.message = status.getMessage();
-        this.code = status.getCode();
-    }
 }
 
--- src/main/java/com/manneung/careerup/domain/base/BaseResponseStatus.java
+++ src/main/java/com/manneung/careerup/domain/base/BaseResponseStatus.java
@@ -1,8 +1,10 @@
 package com.manneung.careerup.domain.base;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 //에러 메시지 모음
+@AllArgsConstructor
 @Getter
 public enum BaseResponseStatus {
     //모듈_에러내용_ERROR
@@ -11,64 +13,45 @@ public enum BaseResponseStatus {
     /**
      * 1000: 기본 요청 성공, 실패
      */
-    SUCCESS(true, 1000, "요청에 성공하였습니다."),
+    SUCCESS(1000, "요청에 성공하였습니다."),
 
-    FAIL(false, 1001, "요청에 실패했습니다."),
+    FAIL(1001, "요청에 실패했습니다."),
 
 
     /**
      * 2000 : User 오류
      */
-    USER_EXISTS_NICKNAME_ERROR(false,2001,"중복된 닉네임입니다."),
+    USER_EXISTS_NICKNAME_ERROR(2001,"중복된 닉네임입니다."),
 
-    USER_FAILED_TO_LOG_IN_ERROR(false, 2002, "로그인에 실패하였습니다."),
+    USER_FAILED_TO_LOG_IN_ERROR(2002, "로그인에 실패하였습니다."),
 
-    USER_NOT_EXIST_USER_IDX_ERROR(false, 2003, "존재하지 않는 유저 idx입니다."),
+    USER_NOT_EXIST_USER_IDX_ERROR(2003, "존재하지 않는 유저 idx입니다."),
 
-    USER_NOT_EXIST_NICKNAME_ERROR(false, 2004, "존재하지 않는 닉네임입니다."),
+    USER_NOT_EXIST_NICKNAME_ERROR(2004, "존재하지 않는 닉네임입니다."),
 
-    USER_NOT_EXIST_EMAIL_ERROR(false, 2005, "존재하지 않는 이메일입니다."),
+    USER_NOT_EXIST_EMAIL_ERROR(2005, "존재하지 않는 이메일입니다."),
 
-    USER_ALREADY_DELETED_ERROR(false, 2006, "이미 탈퇴된 유저입니다."),
+    USER_ALREADY_DELETED_ERROR(2006, "이미 탈퇴된 유저입니다."),
 
+    USER_NOT_EXIST_PROVIDER_ERROR(2007, "제공하지 않는 소셜 로그인입니다."),
 
+    USER_ERROR(2008, "로그인된 유저 찾기 실패"),
 
     /**
      * 3000 : Map 오류
      */
-    MAP_ALREADY_DELETED_ERROR(false, 3001, "이미 삭제된 커리어맵입니다."),
+    MAP_ALREADY_DELETED_ERROR(3001, "이미 삭제된 커리어맵입니다."),
 
-    MAP_NOT_FOUND_IDX_ERROR(false, 3002, "존재하지 않는 커리어맵입니다."),
+    MAP_NOT_FOUND_IDX_ERROR(3002, "존재하지 않는 커리어맵입니다."),
 
-    MAP_FAILED_TO_UPDATE_ERROR(false, 3003, "커리어맵 업데이트에 실패했습니다."),
+    MAP_FAILED_TO_UPDATE_ERROR(3003, "커리어맵 업데이트에 실패했습니다."),
 
-    MAP_EMPTY_LIST_ERROR(false, 3004, "커리어맵 리스트를 찾을 수 없습니다."),
+    MAP_EMPTY_LIST_ERROR( 3004, "커리어맵 리스트를 찾을 수 없습니다."),
 
-    MAP_FAILED_TO_CREATE_ERROR(false, 3005, "커리어맵 작성에 실패했습니다."),
-
-
-
-
-
-
+    MAP_FAILED_TO_CREATE_ERROR( 3005, "커리어맵 작성에 실패했습니다."),
 
     ;
 
-
-
-
-
-
-
-    //contructor
-    private final boolean isSuccess;
     private final int code;
     private final String message;
-
-    private BaseResponseStatus(boolean isSuccess, int code, String message) {
-        //BaseResponseStatus 에서 각 해당하는 코드를 생성자로 맵핑
-        this.isSuccess = isSuccess;
-        this.code = code;
-        this.message = message;
-    }
 }
