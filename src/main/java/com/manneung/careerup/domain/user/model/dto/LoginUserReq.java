123bc80c2ccf90883c98d6e3154ab9daeaf379ae
YuJin Kwon <tokyj515@naver.com>
2022-12-12 22:40:22 +0900

    [modify] oauth 관련

---
+++ src/main/java/com/manneung/careerup/domain/user/model/dto/LoginUserReq.java
@@ -0,0 +1,25 @@
+package com.manneung.careerup.domain.user.model.dto;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@ApiModel(description = "로그인을 위한 요청 객체")
+public class LoginUserReq {
+
+    @NotBlank(message = "회원의 로그인Id를 입력해주세요.")
+    @ApiModelProperty(notes = "로그인 Id를 입력해 주세요.")
+    private String email;
+
+    @NotBlank(message = "회원의 비밀번호를 입력해 주세요.")
+    @ApiModelProperty(notes = "회원의 비밀번호를 입력해 주세요.")
+    private String password;
+
+}
