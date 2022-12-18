0d2c7455543ae727ca753e154cf26b1bbd2f1cb1
YuJin Kwon <tokyj515@naver.com>
2022-12-18 22:02:24 +0900

    [add] map dto 추가

---
+++ src/main/java/com/manneung/careerup/domain/map/model/dto/PatchMapReq.java
@@ -0,0 +1,21 @@
+package com.manneung.careerup.domain.map.model.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+public class PatchMapReq {
+
+    private String title;
+
+
+}
