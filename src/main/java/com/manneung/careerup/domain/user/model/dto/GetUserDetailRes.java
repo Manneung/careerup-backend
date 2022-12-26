2ae9ef71664607078cc75bd26846011ba7910b66
YuJin Kwon <tokyj515@naver.com>
2022-12-26 19:45:25 +0900

    [modify] 로그인 로직 수정

--- src/main/java/com/manneung/careerup/domain/user/model/dto/GetUserDetailRes.java
+++ src/main/java/com/manneung/careerup/domain/user/model/dto/GetUserDetailRes.java
@@ -12,25 +12,30 @@ import lombok.*;
 @Getter
 public class GetUserDetailRes {
     //유저 전체 정보 보여주는 DTO
+    //마이페이지 정보들 전체
 
 
+    //기본 유저 정보
     private String username; //이메일
 
     private String name;
 
+
+    //추가 유저 정보
     private String age;
 
     private String nickname;
 
     private String phone;
 
-
-
     private String picture;
 
     private String job;
 
     private String address;
+
+
+    private String link;
 
     private String univ;
 
@@ -55,6 +60,7 @@ public class GetUserDetailRes {
                 .picture(user.getPicture())
                 .job(user.getJob())
                 .address(user.getAddress())
+                .link(user.getLink())
                 .univ(user.getUniv())
                 .major1(user.getMajor1())
                 .major2(user.getMajor2())
@@ -62,7 +68,6 @@ public class GetUserDetailRes {
                 .interestField2(user.getInterestField2())
                 .interestField3(user.getInterestField3())
                 .build();
-
     }
 
 }
--- src/main/java/com/manneung/careerup/domain/user/model/dto/PatchUserReq.java
+++ src/main/java/com/manneung/careerup/domain/user/model/dto/PatchUserReq.java
@@ -13,22 +13,24 @@ import lombok.Setter;
 @Getter
 public class PatchUserReq {
     //유저 컬럼 중 수정할 수 있는 컬럼만
+    //비밀번호는 따로 처리
 
-    //기본 정보 중 수정(비밀번호 변경은 따로 처리)
+
+    private String age;
+
     private String nickname;
 
     private String phone;
 
-    private String age;
-
-
-
-    //추가 정보 중 수정
     private String picture;
 
     private String job;
 
     private String address;
+
+
+
+    private String link;
 
     private String univ;
 
