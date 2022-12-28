6a1c6fbd8aa9221fca3ed4526c8ebd9142416c4b
YuJin Kwon <tokyj515@naver.com>
2022-12-28 21:12:35 +0900

    [add] 아이템 파일 업로드 api

---
+++ src/main/java/com/manneung/careerup/domain/file/repository/FileRepository.java
@@ -0,0 +1,16 @@
+package com.manneung.careerup.domain.file.repository;
+
+
+import com.manneung.careerup.domain.file.model.File;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface FileRepository extends JpaRepository<File, Integer> {
+
+    List<File> findFilesByItemIdx(int itemIdx);
+
+
+}
---
+++ src/main/java/com/manneung/careerup/domain/file/service/FileService.java
@@ -0,0 +1,50 @@
+package com.manneung.careerup.domain.file.service;
+
+
+import com.manneung.careerup.domain.file.model.File;
+import com.manneung.careerup.domain.file.repository.FileRepository;
+import com.manneung.careerup.global.s3.S3UploaderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
+
+
+@RequiredArgsConstructor
+@Service
+public class FileService {
+
+    private final FileRepository fileRepository;
+
+    private final S3UploaderService s3UploaderService;
+
+
+    //각 활동에 들어간 파일 목록 전체 불러오기(사진, 파일 구분x)
+    public List<File> findFilesByItemIdx(int itemIdx){
+        return fileRepository.findFilesByItemIdx(itemIdx);
+    }
+
+    //파일 업로드
+    public String upload(MultipartFile multipartFile, String bucket, String dirName ) throws IOException {
+        return s3UploaderService.upload(multipartFile, "careerup-bucket", "image");
+    }
+
+    //활동 사진 업로드
+    public String itemPictureUpload(int itemIdx, MultipartFile multipartFile, String bucket, String dirName ) throws IOException {
+        String fileName = s3UploaderService.upload(multipartFile, "careerup-bucket", "image");
+
+        File newFile = new File();
+        newFile.setItemIdx(itemIdx);
+        newFile.setFileType("활동사진");
+        newFile.setFileName(fileName);
+
+        fileRepository.save(newFile);
+
+        return fileName;
+    }
+
+
+}
