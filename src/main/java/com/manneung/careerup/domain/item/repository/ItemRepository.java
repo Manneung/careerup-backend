96238dc27e4254252f8075e24dcf072bd0464319
YuJin Kwon <tokyj515@naver.com>
2023-01-05 01:02:56 +0900

    [refactor] 아이템 순서 변경 api 추가

--- src/main/java/com/manneung/careerup/domain/item/repository/ItemRepository.java
+++ src/main/java/com/manneung/careerup/domain/item/repository/ItemRepository.java
@@ -16,6 +16,7 @@ public interface ItemRepository extends JpaRepository<Item, Integer> {
     //mapidx가 일치한 행을 찾고 count를 기준으로 오름차순으로 정렬한 item리스트를 반환
     List<Item> findAllByMapIdxOrderBySequenceAsc(int mapIdx);
 
+    List<Item> findAllByMapIdx(int mapIdx);
 
     Item findByItemIdx(int itemIdx);
 
