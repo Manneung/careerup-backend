d0adf06e16d3b7cd2ae1ea02935ad2d9b6c84c92
YuJin Kwon <tokyj515@naver.com>
2023-01-18 20:10:31 +0900

    [refactor] cors 세팅

---
+++ src/main/java/com/manneung/careerup/global/config/WebConfig.java
@@ -0,0 +1,18 @@
+package com.manneung.careerup.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+// 스프링 서버 전역적으로 CORS 설정
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:8080", "http://localhost:3000") // 허용할 출처
+                .allowedMethods("GET", "POST") // 허용할 HTTP method
+                .allowCredentials(true) // 쿠키 인증 요청 허용
+                .maxAge(3000); // 원하는 시간만큼 pre-flight 리퀘스트를 캐싱
+    }
+}
