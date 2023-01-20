60daf79f2c559e09133ae100012961313dcd963a
YuJin Kwon <tokyj515@naver.com>
2023-01-20 21:39:21 +0900

    [feat] 리프레시토큰 추가

---
+++ src/main/java/com/manneung/careerup/global/redis/RedisService.java
@@ -0,0 +1,43 @@
+package com.manneung.careerup.global.redis;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.stereotype.Service;
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
+@Service
+@RequiredArgsConstructor
+public class RedisService {
+    private final RedisTemplate<String, String> redisTemplate;
+
+    public void setValues(String key, String data) {
+        ValueOperations<String, String> values = redisTemplate.opsForValue();
+        values.set(key, data);
+    }
+
+    public void setValues(String key, String data, Duration duration) {
+        ValueOperations<String, String> values = redisTemplate.opsForValue();
+        values.set(key, data, duration);
+    }
+
+    public String getValues(String key) {
+        ValueOperations<String, String> values = redisTemplate.opsForValue();
+        return values.get(key);
+    }
+
+    public void deleteValues(String key) {
+        redisTemplate.delete(key);
+    }
+
+    public void saveToken(String userId, String refreshToken, long time){
+        redisTemplate.opsForValue().set(userId, refreshToken, time, TimeUnit.MILLISECONDS);
+        //Redis에 저장 후 만료시간 설정을 위해 자동 삭제처리
+    }
+
+
+
+
+}
