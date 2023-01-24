72fba25d8783a24c955e04bbb9d56e5e19192ca7
YuJin Kwon <tokyj515@naver.com>
2023-01-24 16:14:11 +0900

    [refactor] cors 1

---
+++ src/main/java/com/manneung/careerup/global/config/WebConfig.java
@@ -0,0 +1,23 @@
+package com.manneung.careerup.global.config;
+
+import org.springframework.context.annotation.Configuration;
+
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://3.36.230.165/")
+                .allowedMethods("*")
+                .allowCredentials(false)
+                .maxAge(3000);
+    }
+}
